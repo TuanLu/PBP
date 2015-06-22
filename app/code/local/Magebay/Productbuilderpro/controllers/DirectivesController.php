9c5c518afef5dcdd4c7bae6ab5d32cfbbcdcb6d7
Tuan Lu <tuan.mrbean@gmail.com>
2015-06-22 07:22:17 +0700

    Re struct directives folder

--- app/code/local/Magebay/Productbuilderpro/controllers/DirectivesController.php
+++ app/code/local/Magebay/Productbuilderpro/controllers/DirectivesController.php
@@ -3,13 +3,13 @@ class Magebay_Productbuilderpro_DirectivesController extends Mage_Core_Controlle
 {
     public function pbpGroupAction()
     {
-        echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/pbp-group.phtml")->toHtml();
+        echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/core/pbp-group.phtml")->toHtml();
     }
     public function pbpOptionCollectionAction() {
-        echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/pbp-option-collection.phtml")->toHtml();
+        echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/core/pbp-option-collection.phtml")->toHtml();
     }
     public function pbpOptionDetailsAction() {
-        echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/pbp-option-details.phtml")->toHtml();
+        echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/core/pbp-option-details.phtml")->toHtml();
     }
     // Frontend
     public function pbpLayerCollectionAction() {
@@ -19,7 +19,7 @@ class Magebay_Productbuilderpro_DirectivesController extends Mage_Core_Controlle
         echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/pbp-layer-details.phtml")->toHtml();
     }
     public function pbpMediaAction() {
-        echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/pbp-media.phtml")->toHtml();
+        echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/core/pbp-media.phtml")->toHtml();
     }
     public function selectedLayerAction() {
         echo $this->getLayout()->createBlock("core/template")->setTemplate("productbuilderpro/directives/selected-layer.phtml")->toHtml();
